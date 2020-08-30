7776ca872cb12065bf6561a7baa08c25ab6b5d48
sat98 <50227644+sat98@users.noreply.github.com>
2020-08-31 01:37:51 +0530

    Add files via upload

---
+++ SelectionDemo.java
@@ -0,0 +1,21 @@
+public class SelectionDemo
+{
+	public static void main(String[] args)
+	{
+		int n=0;
+
+		if(n==0)
+		{
+			System.out.println("nothing");
+			System.out.println("Bye");
+		}
+		else if(n%2==0)
+		{
+			System.out.println("Even");
+		}
+		else
+		{
+			System.out.println("Odd");
+		}
+	}
+}
