cecc95756378deae75069ae672e0376e5357bdde
sat98 <50227644+sat98@users.noreply.github.com>
2020-09-02 00:38:33 +0530

    Add files via upload

---
+++ IterationWhile.java
@@ -0,0 +1,15 @@
+public class IterationWhile
+{
+	// while, do while, for, for-each(for unleashed)
+	public static void main(String[] args)
+	{
+		int i=1;// initialisation
+		while(i<=5/*condition*/)
+		{
+			System.out.println("Hello");//prints in multiple lines
+			System.out.print("Hello");// prints in a single line
+			//loop text
+			i++;// increment operation to match the loop
+		}
+	}
+}
