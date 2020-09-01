531a13d5b88e0844969186cd6eb04ab822936dcc
sat98 <50227644+sat98@users.noreply.github.com>
2020-09-01 22:56:46 +0530

    Add files via upload

---
+++ Tricks1.java
@@ -0,0 +1,12 @@
+
+public class Tricks1
+{
+	public static void main(String[] args)
+	{
+		if(System.out.printf("Hello World")==null)
+		{
+			// trick to print "Hello World" without
+			// using a semicolon ";"
+		}
+	}
+}
